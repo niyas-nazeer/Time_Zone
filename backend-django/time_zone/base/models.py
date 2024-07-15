85e231fa9a75c2a3e903f19b8fc995de358ce16d
niyas-nazeer <niyasnazeer719@gmail.com>
2024-07-15 12:57:17 +0530

    models created 15/07/24

--- backend-django/time_zone/base/models.py
+++ backend-django/time_zone/base/models.py
@@ -9,4 +9,40 @@ class customer(models.Model):
     gender=models.CharField(max_length=1)
     address_1=models.TextField()
     address_2=models.TextField()
-    cart_id=models.IntegerField()
+
+class admin(models.model):
+    admin_id=models.AutoField(primary_key=True)
+    first_name=models.CharField(max_length=255)
+    last_name=models.CharField(max_length=255)
+    role=models.CharField(max_length=255)
+    employee_id=models.IntegerField()
+
+class seller(models.model):
+    seller_id=models.AutoField(primary_key=True)
+    swller_name=models.CharField(max_length=255)
+    legal_name=models.CharField(max_length=255)
+    contact_person=models.CharField(max_length=255)
+    alter_mobile=models.CharField(max_length=14)
+
+class order(models.model):
+    order_id=models.AutoField(primary_key=True)
+    customer_id=models.IntegerField()
+    order_date=models.DateTimeField()
+    order_status=models.CharField(max_length=10)
+    transaction_id=models.IntegerField()
+
+class order_details(models.model):
+    order_details_id=models.AutoField(primary_key=True)
+    order_id=models.IntegerField()
+    product_id=models.IntegerField()
+    product_quantity=models.IntegerField()
+    total_amount=models.DecimalField(7,2)
+
+class cart(models.model):
+    product_id=models.IntegerField()
+    customer_id=models.IntegerField()
+    product_quantity=models.IntegerField()
+
+    class meta:
+        unique_togethor = ('customer_id','product_id')
+
